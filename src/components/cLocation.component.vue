b266b55a618b15359a35b4c47da221920c631690
vikiborodienko <borodienko.v@gmail.com>
2022-08-12 09:08:12 +0200

    fix cr logo component

--- src/components/cLocation.component.vue
+++ src/components/cLocation.component.vue
@@ -7,6 +7,7 @@
   <div class='flex items-center justify-center mt-1 relative'>
     <img src='../assets/icons/pin.png'
          alt='location-pin'>
+
       <p class='-mb-1 mx-2 font-montserrat font-bold'>Dhaka, Banassre</p>
   </div>
 </template>
